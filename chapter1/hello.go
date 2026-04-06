06a357bf4100255c22dd9db5986195e46e1a7415
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2026-04-06 21:40:50 +0630

    first commit

---
+++ chapter1/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	a := 3 / 2
+	fmt.Println("The value of a is ", a)
+	//a = "Hello"
+	//fmt.Println("The value of a is ", a)
+}
